25161b112971f9315c3f23c7fed3ceffeff8c18e
mullagoziev@gmail.com <Fahriddin@WIN-U2794NDE3MO>
2024-07-25 23:23:57 -0400

    Path variables

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,19 @@ public class HelloWorldController {
 	// /hello-world
 
 	// "Hello World"
-	@GetMapping(path="hello-world")
+	@GetMapping(path = "hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
-	
-	@GetMapping(path="hello-world-bean")
+
+	@GetMapping(path = "hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+
+	// /hello-world/path-variable/Ranga
+	@GetMapping(path = "hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
