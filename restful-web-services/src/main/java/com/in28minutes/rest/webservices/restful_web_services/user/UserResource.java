bee5443860f4ce748a0e25d1994eec59b7722778
Fakhriddin Mullagoziev <mullagoziev@gmail.com>
2024-07-30 11:16:36 -0400

    Response status

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -2,10 +2,12 @@ package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,10 +29,11 @@ public class UserResource {
 	public User retreiveUser(@PathVariable int id) {
 		return service.findOne(id);
 	}
-	
+
 	// POST/users
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 		service.save(user);
+		return ResponseEntity.created(null).build();
 	}
 }
