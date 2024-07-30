c77e81baee0b3c2ab4bde1ea0adae353181585d3
mullagoziev@gmail.com <Fahriddin@WIN-U2794NDE3MO>
2024-07-30 05:58:02 -0400

    save

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -8,19 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserResource {
+
 	private UserDaoService service;
+
 	public UserResource(UserDaoService service) {
-		this.service=service;
+		this.service = service;
 	}
-	
+
 	// GET /users
 	@GetMapping("/users")
-	public List<User> retreiveAllUsers(){
+	public List<User> retreiveAllUsers() {
 		return service.findAll();
 	}
-	
+
 	@GetMapping("/users/{id}")
-	public User retreiveUser(@PathVariable int id){
+	public User retreiveUser(@PathVariable int id) {
 		return service.findOne(id);
 	}
 }
