35dc36fc1ea134d3fed772559a9008b60a7378a5
Fakhriddin Mullagoziev <mullagoziev@gmail.com>
2024-08-10 00:11:58 +0500

    Creating Post Entity with Many to One Relationship with User Entity

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -1,13 +1,16 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -31,6 +34,10 @@ public class User {
 	@Past(message="Birth date should be in the past")
 //	@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> post;
 	public User(int id, String name, LocalDate birthDate) {
 		super();
 		this.id = id;
