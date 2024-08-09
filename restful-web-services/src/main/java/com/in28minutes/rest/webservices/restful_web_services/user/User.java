ffb5f851c4331dece737798d1f4137ecabdf80ac
mullagoziev@gmail.com <Fahriddin@WIN-U2794NDE3MO>
2024-08-09 05:47:37 -0400

    Static filtering @JsonProperty, @JsonIgnor and @JsonIgnorProperty

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -11,8 +13,10 @@ import jakarta.validation.constraints.Size;
 public class User {
 	private Integer id;
 	@Size(min=2,message="Name should have at least 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	@Past(message="Birth date should be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	public User(int id, String name, LocalDate birthDate) {
 		super();
