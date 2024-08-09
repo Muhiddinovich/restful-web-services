42ba238cb57af5fe3cd598f74d2900e10846b973
Fakhriddin Mullagoziev <mullagoziev@gmail.com>
2024-08-09 23:26:54 +0500

    - Enhancing REST API to connect to H2 using JPA and Hibernate

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -14,16 +14,22 @@ import jakarta.validation.constraints.Size;
 /**
  * 
  */
+
+
 @Entity(name="user_details")
 public class User {
+	protected User() {
+		
+	}
+	
 	@Id
 	@GeneratedValue
 	private Integer id;
 	@Size(min=2,message="Name should have at least 2 characters")
-	@JsonProperty("user_name")
+//	@JsonProperty("user_name")
 	private String name;
 	@Past(message="Birth date should be in the past")
-	@JsonProperty("birth_date")
+//	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	public User(int id, String name, LocalDate birthDate) {
 		super();
