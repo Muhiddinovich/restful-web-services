cc0b7b8f306e00044217690d6f062f611a5c1e0b
mullagoziev@gmail.com <Fahriddin@WIN-U2794NDE3MO>
2024-08-13 06:41:09 -0400

    Implementing a POST API to create a Post for a user

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/Post.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/Post.java
@@ -7,12 +7,14 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Post {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	@Size(min=10)
 	private String description;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
@@ -30,9 +32,16 @@ public class Post {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
 	@Override
 	public String toString() {
 		return "Post [id=" + id + ", description=" + description + "]";
 	}
+	
 	
 }
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserJpaResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserJpaResource.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.in28minutes.rest.webservices.restful_web_services.jpa.PostRepository;
 import com.in28minutes.rest.webservices.restful_web_services.jpa.UserRepository;
 
 import jakarta.validation.Valid;
@@ -26,9 +27,11 @@ import jakarta.validation.Valid;
 public class UserJpaResource {
 
 	private UserRepository repository;
+	private PostRepository postRepository;
 
-	public UserJpaResource(UserRepository repository) {
+	public UserJpaResource(UserRepository repository, PostRepository postRepository) {
 		this.repository = repository;
+		this.postRepository = postRepository;
 	}
 
 	// GET /users
@@ -73,5 +76,18 @@ public class UserJpaResource {
 			throw new UserNotFoundException("id:" + id);
 		}
 		return user.get().getPost();
+	}
+
+	@PostMapping("/jpa/users/{id}/posts")
+	public ResponseEntity<Object> createPostsForUser(@PathVariable int id, @Valid @RequestBody Post post) {
+		Optional<User> user = repository.findById(id);
+		if (user.isEmpty()) {
+			throw new UserNotFoundException("id:" + id);
+		}
+		post.setUser(user.get());
+		Post savedPost = postRepository.save(post);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedPost.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
